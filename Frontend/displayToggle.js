2aaa7cc680ba8e7e31019e91d7dd476b9d2369c0
Youcef <youcefcopy@gmail.com>
2025-10-24 20:11:15 +0100

    JS functionality has been added

---
+++ Frontend/displayToggle.js
@@ -0,0 +1,13 @@
+// ! Mainly, we are going to use jaavscript to add and remove a class (targeting display: none)
+// ! We are going to use differ in the script tag, so it will work after html has been loaded
+
+let SignUp = document.getElementById(`SignUp`);
+const Login = document.getElementById(`Login`);
+
+
+
+const hideSignup = () => {
+    document.body.classList.add(``)
+}
+
+Login.onclick = hideSignup;
